ae645a62a6d985fee221a043642e44f32e492172
Rishabh Sahu <rishabhsahu13873@gmail.com>
2025-05-20 13:24:47 +0530

    get me chai project added

--- src/pages/projectsData.js
+++ src/pages/projectsData.js
@@ -6,11 +6,21 @@ import ecomSite from "../assets/ecomSite.png"
 import noteverse from  "../assets/noteverse.png"
 import tictac from "../assets/tictactoe.png"
 import cafe from "../assets/cafe.png"
+import chai from "../assets/chai.png"
 
 
 const projectsData = [
   {
     id: 1,
+    name: "Get me a Chai",
+    discription:"A simple donation web app where users can support creators by buying them a virtual chai using Razorpay.",
+    image: chai,
+    tags:" #Next.js #NextAuth.js #Shadcn #MongoDB ",
+    link: "https://get-me-chai-roan.vercel.app/",
+    gitlink:"https://github.com/Rishabh05Sahu/Get-me-chai"
+  },
+  {
+    id: 2,
     name: "Cafe Ordering Site",
     discription:"Crafted a notes management system that supports 40+ subjects and PYQs, simplifying academic resource access.",
     image: cafe,
@@ -19,7 +29,7 @@ const projectsData = [
     gitlink:"https://github.com/Rishabh05Sahu/cafe-ordering"
   },
   {
-    id: 2,
+    id: 3,
     name: "Note Verse",
     discription:"Crafted a notes management system that supports 40+ subjects and PYQs, simplifying academic resource access.",
     image: noteverse,
@@ -28,7 +38,7 @@ const projectsData = [
     gitlink:"https://github.com/Rishabh05Sahu/noteverse"
   },
   {
-    id: 3,
+    id: 4,
     name: "Apni Dukaan",
     discription:"Developed an e-commerce platform featuring 30+ products across 3 categories with advanced filtering capabilities.",
     image: ecomSite,
@@ -37,7 +47,7 @@ const projectsData = [
     gitlink:"https://github.com/Rishabh05Sahu/e-com-site"
   },
   {
-    id: 4,
+    id: 5,
     name: "Customizable Tic Tac Toe",
     discription:"A simple and interactive Tic Tac Toe game built with React, allowing users to play against each other with real-time updates and a smooth user interface.",
     image: tictac,
@@ -46,7 +56,7 @@ const projectsData = [
     gitlink:"https://github.com/Rishabh05Sahu/tic-tac-toe"
   },
   {
-    id: 5,
+    id: 6,
     name: "Drum Kit",
     discription:"Created an interactive drum kit using HTML, CSS, and JavaScript for an engaging musical experience.",
     image: drumKit,
@@ -55,7 +65,7 @@ const projectsData = [
     gitlink:"https://githuabh05Sahu/Drum-Kitb.com/Rish"
   },
   {
-    id: 6,
+    id: 7,
     name: "Pickup Line Generator",
     discription:"Created a fun and interactive pickup line generator using HTML, CSS, and JavaScript.",
     image: pickup,
@@ -64,7 +74,7 @@ const projectsData = [
     gitlink:"https://github.com/Rishabh05Sahu/pickup-line-generator"
   },
     {
-      id: 7,
+      id: 8,
       name: "Hotel Booking Site",
       discription:"Developed a user-friendly hotel booking website using HTML and CSS, enabling seamless reservation experiences.",
       image: hotel,
